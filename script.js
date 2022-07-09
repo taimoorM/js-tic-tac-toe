32401e0c46f4cf215dffe3dc3e1c94f5e7627c29
Taimoor <taimoor.m89@gmail.com>
2022-07-09 05:36:30 -0400

    added function to display winner

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const boxes = document.querySelectorAll(".box");
+const bottomContainer = document.querySelector(".bottomContainer");
 
 //when user clicks a box, display an x on it
 //if the box has a x or o do nothing
 //add an O to a random box on the board unless the player has 2 in a row
 //if player or computer has 3 in a row they win
-//display is player wins or loses
+//display iif player wins or loses or draws
 //show reset button
 //win Cases
 // 123, 456, 789
@@ -13,7 +14,7 @@ const boxes = document.querySelectorAll(".box");
 
 const playerOne = "X";
 const playerTwo = "O";
-const WINNING_COMBINATIONS = [
+const winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -25,13 +26,33 @@ const WINNING_COMBINATIONS = [
 ];
 let playerOneTurn = true;
 let gameOver = false;
+let gameDraw = false;
+let currentPlayer = playerOne;
 
 for (let i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener("click", function () {
-    const currentPlayer = playerOneTurn ? playerOne : playerTwo;
+    currentPlayer = playerOneTurn ? playerOne : playerTwo;
     this.textContent = currentPlayer;
     playerOneTurn = !playerOneTurn;
   });
 }
 
-function checkWin() {}
+function displayWinner(player) {
+  //display winning message based on player
+  bottomContainer.innerHTML = `<h2>${player} Wins!</h2>`;
+}
+
+function checkWin() {
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const winningCombination = winningCombinations[i];
+    let a = boxes[0].textContent;
+    let b = boxes[1].textContent;
+    let c = boxes[2].textContent;
+
+    if (a || b || c) {
+      if (a === b && b === c) {
+        gameOver = true;
+      }
+    }
+  }
+}
